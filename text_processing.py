8212a18020d9c5f27ca4555943cfbb917791af20
AYŞE DUMAN <48975696+AYSE-DUMAN@users.noreply.github.com>
2024-06-05 22:57:19 +0300

    Update text_processing.py

--- text_processing.py
+++ text_processing.py
@@ -1,12 +1,68 @@
 # -*- coding: utf-8 -*-
 """
+TEXT PROCESSING 
 @author: AyseDuman
 """
+
 import re
 import string
 import nltk
 from nltk.corpus import stopwords
 nltk.download('stopwords')
+
+
+location_mapping = {
+    'USA': 'United States',
+    'United States': 'United States',
+    'San Francisco': 'United States',
+    'UK': 'United Kingdom',
+    'London': 'United Kingdom',
+    'Washington, DC': 'United States',
+    'Washington, D.C.': 'United States',
+    'Chicago, IL': 'United States',
+    'New York': 'United States',
+    'Los Angeles, CA': 'United States',
+    'California, USA': 'United States',
+    'Los Angeles': 'United States',
+    'Nashville, TN': 'United States',
+    'Earth': 'Unknown',
+    'California': 'United States',
+    'Mumbai': 'India',
+    'Toronto': 'Canada',
+    'Sacramento, CA': 'United States',
+    'New York, NY': 'United States',
+    'New York City': 'United States',
+    'Denver, Colorado': 'United States',
+    'San Francisco, CA ': 'United States',
+    'US': 'United States',
+    'San Francisco, CA': 'United States',
+    'Oklahoma City, OK': 'United States',
+    'Atlanta, GA': 'United States',
+    'London, UK': 'United Kingdom',
+    'ss': 'Unknown',
+    'NYC': 'United States',
+    'Florida': 'United States',
+    'Everywhere': 'Unknown',
+    'Worldwide': 'Unknown',
+    '304': 'Unknown',
+    'London, England': 'United Kingdom',
+    'Seattle': 'United States',
+    'Texas': 'United States',
+    'Chicago': 'United States',
+    'Dallas, TX': 'United States',
+    'Pennsylvania, USA': 'United States',
+    'Manchester': 'United Kingdom',
+    'San Diego, CA': 'United States',
+    'Morioh, Japan': 'Japan'
+}
+
+
+# Function to map locations in a DataFrame using the provided mapping dictionary
+def map_locations(df, location_mapping):
+    # Apply the mapping to the location column
+    df['location'] = df['location'].map(location_mapping).fillna(df['location'])
+    return df
+
 
 def clean_text(text):
     # Remove line breaks
@@ -28,6 +84,7 @@ def clean_text(text):
     text = " ".join([word for word in text.split() if word.lower() not in stop_words])
     return text
 
+
 def extract_entities(tweet):
     hashtags = " ".join(re.findall(r"#(\w+)", tweet)) or 'no'
     mentions = " ".join(re.findall(r"@(\w+)", tweet)) or 'no'
@@ -36,3 +93,9 @@ def extract_entities(tweet):
     time = 'yes' if re.search(r'\b\d{1,2}:\d{2}(?::\d{2})?\b', tweet) else 'no'
     rt = 'yes' if re.search(r'\bRT\b', tweet) else 'no'
     return hashtags, mentions, links, numbers, time, rt
+
+
+def process_text(df):
+    df['text_clean'] = df['text'].apply(lambda x: clean_text(x))
+    df[['hashtags', 'mentions', 'links', 'numbers', 'time', 'rt']] = df['text'].apply(lambda x: pd.Series(extract_entities(x)))
+    return df
